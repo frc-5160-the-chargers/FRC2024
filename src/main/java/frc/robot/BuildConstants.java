5f9eaa7515ff842f97be61f138528d18a6e22fe0
DanielRocksUrMom <108989218+DanielRocksUrMom@users.noreply.github.com>
2024-01-02 08:22:23 -0500

    Changed back to main.java due to issues with running on VScode

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "FRC2024";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 5;
-  public static final String GIT_SHA = "45efed12ae8594050edf7115ecbb4e44320c540d";
-  public static final String GIT_DATE = "2023-12-31 12:51:48 EST";
+  public static final int GIT_REVISION = 6;
+  public static final String GIT_SHA = "4aff577c9aa4b3cb6fdd1b4ed810c529a0dab8e6";
+  public static final String GIT_DATE = "2024-01-01 20:14:21 EST";
   public static final String GIT_BRANCH = "master";
-  public static final String BUILD_DATE = "2024-01-01 18:39:39 EST";
-  public static final long BUILD_UNIX_TIME = 1704152379998L;
+  public static final String BUILD_DATE = "2024-01-01 20:22:15 EST";
+  public static final long BUILD_UNIX_TIME = 1704158535118L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
