0635b1d1aed30691b1cf20feb8913b6d5b9c7e20
DanielRocksUrMom <108989218+DanielRocksUrMom@users.noreply.github.com>
2024-01-24 10:53:21 -0500

    More renaming clarity

--- src/main/java/frc/chargers/constants/DiffDriveControlData.kt
+++ src/main/java/frc/chargers/constants/DiffDriveControlData.kt
@@ -1,32 +1,10 @@
 @file:Suppress("RedundantVisibilityModifier", "unused") 
 package frc.chargers.constants
 
-import com.batterystaple.kmeasure.quantities.Distance
-import com.batterystaple.kmeasure.quantities.Length
-import com.batterystaple.kmeasure.units.inches
 import com.pathplanner.lib.util.ReplanningConfig
 import frc.chargers.controls.feedforward.AngularMotorFFEquation
 import frc.chargers.controls.pid.PIDConstants
 
-/**
- * A class used to hold constants for an [frc.chargers.hardware.subsystems.differentialdrive.EncoderDifferentialDrivetrain].
- */
-public data class DiffDriveHardwareData(
-    val invertMotors: Boolean = false,
-    val gearRatio: Double = DEFAULT_GEAR_RATIO,
-    val wheelDiameter: Length,
-    val width: Distance,
-){
-    public companion object{
-        public fun andyMark(invertMotors: Boolean = false): DiffDriveHardwareData =
-            DiffDriveHardwareData(
-                invertMotors,
-                10.71,
-                6.inches,
-                27.inches
-            )
-    }
-}
 
 /**
  * A convenience class for holding control parameters of an [frc.chargers.hardware.subsystems.differentialdrive.EncoderDifferentialDrivetrain].
--- src/main/java/frc/chargers/constants/SwerveHardwareData.kt
+++ src/main/java/frc/chargers/constants/SwerveHardwareData.kt
@@ -1,54 +1,16 @@
-@file:Suppress("RedundantVisibilityModifier", "unused") 
+@file:Suppress("unused")
 package frc.chargers.constants
 
-import com.batterystaple.kmeasure.dimensions.AngleDimension
-import com.batterystaple.kmeasure.quantities.*
+import com.batterystaple.kmeasure.quantities.Length
+import com.batterystaple.kmeasure.quantities.MomentOfInertia
+import com.batterystaple.kmeasure.quantities.Velocity
 import com.batterystaple.kmeasure.units.inches
-import com.pathplanner.lib.util.ReplanningConfig
-import frc.chargers.controls.feedforward.AngularMotorFFEquation
-import frc.chargers.controls.motionprofiling.AngularMotionProfile
-import frc.chargers.controls.pid.PIDConstants
-import frc.chargers.utils.Precision
-
-/**
- * A class that holds Control data for a [frc.chargers.hardware.subsystems.swervedrive.EncoderHolonomicDrivetrain].
- * This includes PID constants, feedforward, and path re-planning configs.
- */
-public data class SwerveControlData(
-    val azimuthControl: SwerveAzimuthControl,
-    val velocityPID: PIDConstants,
-    val velocityFF: AngularMotorFFEquation,
-    val openLoopDiscretizationRate: Double = 2.0,
-    val closedLoopDiscretizationRate: Double = 1.0,
-    val robotRotationPID: PIDConstants = PIDConstants(0.3,0,0),
-    val robotTranslationPID: PIDConstants = PIDConstants(0.3,0,0),
-    val pathReplanConfig: ReplanningConfig = ReplanningConfig()
-)
-
-
-public sealed class SwerveAzimuthControl(
-    val pidConstants: PIDConstants,
-    val precision: Precision<AngleDimension> = Precision.AllowOvershoot
-){
-    class PID(
-        pidConstants: PIDConstants,
-        precision: Precision<AngleDimension> = Precision.AllowOvershoot
-    ): SwerveAzimuthControl(pidConstants, precision)
-
-    class ProfiledPID(
-        pidConstants: PIDConstants,
-        precision: Precision<AngleDimension> = Precision.AllowOvershoot,
-        val motionProfile: AngularMotionProfile,
-        val ffEquation: AngularMotorFFEquation = AngularMotorFFEquation(0.0,0.0)
-    ): SwerveAzimuthControl(pidConstants, precision)
-}
-
 
 /**
  * A class that holds Hardware constants for a [frc.chargers.hardware.subsystems.swervedrive.EncoderHolonomicDrivetrain].
  * This includes Track width, wheelbase, inertia, turn motor inversion, and max speed.
  */
-public data class SwerveHardwareData(
+data class SwerveHardwareData(
     val invertTurnMotors: Boolean = false,
     // offsets the encoder readings to theoretically improve odometry accuracy
     val couplingRatio: Double? = null,
@@ -58,19 +20,19 @@ public data class SwerveHardwareData(
     val driveInertiaMoment: MomentOfInertia = DEFAULT_SWERVE_DRIVE_INERTIA,
     val maxModuleSpeed: Velocity = DEFAULT_MAX_MODULE_SPEED,
     val wheelDiameter: Length,
-    val trackWidth: Distance,
-    val wheelBase: Distance
+    val trackWidth: Length,
+    val wheelBase: Length
 ){
-    public companion object{
+    companion object{
         /**
          * Creates a [SwerveHardwareData] instance with auto-completed constants
          * related to MK4i L2 swerve modules.
          */
-        public fun mk4iL2(
+        fun mk4iL2(
             maxModuleSpeed: Velocity = DEFAULT_MAX_MODULE_SPEED,
             useCouplingRatio: Boolean = true,
-            trackWidth: Distance,
-            wheelBase: Distance,
+            trackWidth: Length,
+            wheelBase: Length,
             turnInertiaMoment: MomentOfInertia = DEFAULT_SWERVE_TURN_INERTIA,
             driveInertiaMoment: MomentOfInertia = DEFAULT_SWERVE_DRIVE_INERTIA,
         ): SwerveHardwareData = SwerveHardwareData(
@@ -90,11 +52,11 @@ public data class SwerveHardwareData(
          * Creates a [SwerveHardwareData] instance with auto-completed constants
          * related to MK4i L3 swerve modules.
          */
-        public fun mk4iL3(
+        fun mk4iL3(
             maxModuleSpeed: Velocity = DEFAULT_MAX_MODULE_SPEED,
             useCouplingRatio: Boolean = true,
-            trackWidth: Distance,
-            wheelBase: Distance,
+            trackWidth: Length,
+            wheelBase: Length,
             turnInertiaMoment: MomentOfInertia = DEFAULT_SWERVE_TURN_INERTIA,
             driveInertiaMoment: MomentOfInertia = DEFAULT_SWERVE_DRIVE_INERTIA,
         ): SwerveHardwareData = SwerveHardwareData(
@@ -114,13 +76,13 @@ public data class SwerveHardwareData(
          * Creates a [SwerveHardwareData] instance with auto-completed constants
          * related to MK4i L1 swerve modules.
          */
-        public fun mk4iL1(
+        fun mk4iL1(
             maxModuleSpeed: Velocity = DEFAULT_MAX_MODULE_SPEED,
             useCouplingRatio: Boolean = true,
             turnInertiaMoment: MomentOfInertia = DEFAULT_SWERVE_TURN_INERTIA,
             driveInertiaMoment: MomentOfInertia = DEFAULT_SWERVE_DRIVE_INERTIA,
-            trackWidth: Distance,
-            wheelBase: Distance
+            trackWidth: Length,
+            wheelBase: Length
         ): SwerveHardwareData = SwerveHardwareData(
             invertTurnMotors = true,
             if (useCouplingRatio) 50.0 / 14.0 else null,
