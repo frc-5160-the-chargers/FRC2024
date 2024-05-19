c37057f0384d7155a67e6f087b12abc7ee880387
DanielRocksUrMom <108989218+DanielRocksUrMom@users.noreply.github.com>
2024-05-19 18:53:27 -0400

    Added experimental datalogging for NT entries

--- src/main/kotlin/frc/chargers/framework/LogLatency.kt
+++ src/main/kotlin/frc/chargers/framework/LogLatency.kt
@@ -5,6 +5,10 @@ import edu.wpi.first.wpilibj.Timer
 
 /**
  * Logs the latency of a certain function.
+ * There is no runtime overhead for logging latency like this.
+ *
+ * @param identifier: The namespace of which latency is logged.
+ * @param function: The function that will be logged.
  */
 inline fun <T> Loggable.logLatency(identifier: String, function: () -> T): T{
     val startTime = Timer.getFPGATimestamp()
